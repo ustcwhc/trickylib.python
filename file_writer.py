4f453877a93cd75a4c04f99141ae886155a87f23
ustcwhc <ustcwhc@gmail.com>
2014-07-04 22:59:09 +0800

    test commands at home pc by ssh

--- file_writer.py
+++ file_writer.py
@@ -24,4 +24,4 @@ if __name__ == '__main__':
     file = 'poem2.txt'
     input_list = [[1,2,3],[4,5.6],[7,8,9]]
     print_double_list(file, input_list)
-    print "Finished"
+    print "Finished Baby!"
